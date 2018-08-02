27ed3d812dbbbc341a180fa7d93f704937e334bb
Michael Petychakis <mpetyx@gmail.com>
2018-08-02 19:48:44 +0300

    uploading examples of the pyxel library

--- examples/02_jump_game.py
+++ examples/02_jump_game.py
@@ -1,10 +1,5 @@
-__author__ = 'mpetyx (Michael Petychakis)'
-__version__ = "1.0.0"
-__maintainer__ = "Michael Petychakis"
-__email__ = "hello@apilama.com"
-__status__ = "Production"
-
 from random import randint
+
 import pyxel
 
 
